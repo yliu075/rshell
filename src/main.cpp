c4917547caf16db4a80bdb50c0411905f1d02331
Yun Liu <yliu075@ucr.edu>
2015-01-23 04:38:11 +0000

    modded main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,25 +30,52 @@ void rshell()
         cmdstring.erase(0,1);
     }
     if (cmdstring.size() > 3 && cmdstring.substr(0,4) == "exit") exit(0);
-    
-    //cout << cmdstring;
-    
-    vector<string> tokens;
-    tokenizer<> tok(cmdstring);
-    for (tokenizer<>::iterator iter = tok.begin(); iter != tok.end(); iter++) {
-        tokens.push_back(*iter);
-    //    cout << *iter << endl;
+    while (cmdstring.find(' ') != string::npos) {
+        cmdstring.erase(cmdstring.find(' '));
     }
-    
+    cout << "cmdstring: "<< cmdstring << endl;
+    vector<string> tokens;
+    char_separator<char> dash("","-");
+    tokenizer<char_separator<char> > tok(cmdstring, dash);
+    for (tokenizer<char_separator<char> >::iterator iter = tok.begin(); iter != tok.end(); iter++) {
+        tokens.push_back(*iter);
+        cout << *iter << endl;
+    }
+    for (size_t i = 0; i < tokens.size(); i++) {
+        cout << tokens.at(i) << endl;
+    }
     //const char *str = cmdstring.c_str();
     //char *cmd;
     // char **arg = NULL; 
     // cmd = strtok (const_cast<char*>(str)," ;");
     // arg[0] = strtok (const_cast<char*>(str)," ;"); 
-    
     //arg = arg + ' ' + strtok (const_cast<char*>(str)," ;"); 
+    vector<string> tokensWithFlags;
     
+    for (size_t h = 0; h < tokens.size(); h++) {
+        if (tokens.at(h) == "-" &&  (h + 1) < tokens.size()) {
+            string temp = tokens.at(h) + tokens.at(h + 1);
+            tokensWithFlags.push_back(temp);
+            h++;
+        }
+        else tokensWithFlags.push_back(tokens.at(h));
+    }
+    for (size_t i = 0; i < tokensWithFlags.size(); i++) {
+        cout << tokensWithFlags.at(i) << endl;
+    }
+    //const char *cmd = tokens.at(0).c_str();
+    char **ARGV = new char*[tokensWithFlags.size() + 1];
     
+    for (size_t j = 0; j < tokensWithFlags.size(); j++) {
+        ARGV[j] = new char[tokensWithFlags.at(j).size() + 1];
+        strcpy(ARGV[j], tokensWithFlags.at(j).c_str());
+        ARGV[tokensWithFlags.size()] = 0;
+    }
+    string CMD = ARGV[0];
+    cout << "CMD: " << CMD << endl;
+    int pass = execvp(ARGV[0], ARGV);
+    perror("error in execvp");
+    cout << pass;
     
     
     // int pid = fork();
