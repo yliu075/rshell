ae2e2cfa779b868fd203c4e349f1dd6fb15804a6
Yun Liu <yliu075@ucr.edu>
2015-02-06 22:34:39 +0000

    modded makefile, added ls.cpp

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-all:
+all: bin
 	g++ -std=c++11 -Wall -Werror -ansi -pedantic src/main.cpp -o bin/rshell
+	g++ -std=c++11 -Wall -Werror -ansi -pedantic src/ls.cpp -o bin/ls
 rshell:
 	g++ -std=c++11 -Wall -Werror -ansi -pedantic src/main.cpp -o bin/rshell
+ls:
+	g++ -std=c++11 -Wall -Werror -ansi -pedantic src/ls.cpp -o bin/ls
 bin:
 	mkdir bin
---
+++ src/ls.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <string.h>
+#include <string>
+#include <boost/tokenizer.hpp>
+#include <vector>
+#include <queue>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
+using namespace boost;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
